3520d3690c10ccaa728900621d20a1b79fc411af
MattSouza14 <mateussouzabr1@gmail.com>
2024-10-03 17:18:02 -0300

    Adição de arquivos iniciais

---
+++ app/src/main/java/com/example/clinicproject/Passo3.java
@@ -0,0 +1,36 @@
+package com.example.clinicproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Passo3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_passo3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void proximaTela3(View view){
+        Intent terceira_tela = new Intent(this,Passo4.class);
+        startActivity(terceira_tela);
+
+    }
+    public void telaAnterior3(View view){
+        Intent primeira_tela = new Intent(this,Passo2.class);
+        startActivity(primeira_tela);
+
+    }
+}
---
+++ app/src/main/java/com/example/clinicproject/Passo4.java
@@ -0,0 +1,32 @@
+package com.example.clinicproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Passo4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_passo4);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void telaAnterior4(View view){
+        Intent primeira_tela = new Intent(this,Passo3.class);
+        startActivity(primeira_tela);
+
+    }
+}
