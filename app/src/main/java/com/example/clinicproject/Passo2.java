e56bfb4ece2290ed9991107af9d72cdc8e8d9770
MattSouza14 <mateussouzabr1@gmail.com>
2024-10-13 15:31:11 -0300

    Inicio da tela 2, ajuste em fontes, adição de spinners e populando array

--- app/src/main/java/com/example/clinicproject/Passo2.java
+++ app/src/main/java/com/example/clinicproject/Passo2.java
@@ -3,6 +3,8 @@ package com.example.clinicproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,42 @@ public class Passo2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Esse array é para as especialidades (não alterar o id, está dando erro ao trocar o id)
+        Spinner spinnerPlano = findViewById(R.id.spinner2);
+        String[] especialidades = {
+                "Escolha uma Plano !!",
+                "AMIL-ONE -S1500",
+                "AMIL-REDE - 100 A 450 ",
+                "AMIL-REDE - 500 A 900",
+                "LIV SAUDE",
+                "MEDSERVICE",
+                "PARTICULAR",
+                "PETROBRAS BR",
+                "PETROBRAS DISTRIBUIDORA",
+                "POPULAR",
+                "POSTAL SAUDE",
+                "PS",
+                "RECEITA FEDERAL",
+                "SAUDE BRADESCO",
+                "SULAMERICA",
+                "UNIMED - PESSOA FISICA",
+                "VAI BEM",
+                "UNIMED - PESSOA FISICA - MULTIPLAN/ESTILO/MULTIMAX/INTERCÂMBIOS/SAULUTE MAX",
+                "VALE SAÚDE SEMPRE"
+        };
+        ArrayAdapter<String> especialidade = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, especialidades);
+        especialidade.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPlano.setAdapter(especialidade);
+
+        Spinner spinnerMedicos = findViewById(R.id.spinnerMedico);
+        String [] medicos = {
+                "Nabel Saldanha"
+
+
+        };
+        ArrayAdapter<String> medico = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, medicos);
+        medico.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMedicos.setAdapter(medico);
     }
     public void trocar(View view){
         Intent terceira_tela = new Intent(this,Passo3.class);
