66cd8c216efbc77738958ae1f293f87a75197bd2
MattSouza14 <mateussouzabr1@gmail.com>
2024-10-12 22:17:43 -0300

    Ajustes na tela 1 e adição de array para mostrar as especialidades(ainda a fazer: botão e ajustes final no design)

--- app/src/main/java/com/example/clinicproject/MainActivity.java
+++ app/src/main/java/com/example/clinicproject/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.clinicproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Spinner spinnerEspecialidades = findViewById(R.id.spinnerEspecialidades);
+        String[] especialidades = {
+                "Escolha uma Especialidade",
+                "Alergologista",
+                "Cardiopediatra",
+                "Cirurgião Torácico",
+                "Nutricionista",
+                "Otorrinolaringologista",
+                "Pediatria",
+                "Pneumologista",
+                "Pneumopediatra"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, especialidades);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEspecialidades.setAdapter(adapter);
     }
     public void trocar(View view){
         Intent segunda_tela = new Intent(this,Passo2.class);
