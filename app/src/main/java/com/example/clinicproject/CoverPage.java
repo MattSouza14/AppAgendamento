6ef28f10ab4fd3820a11e3a733b3e7441e78db81
yuriy <yuriyvieira@gmail.com>
2024-10-23 12:44:31 -0300

    Adicionado uma tela de apresentação antes do primeiro passo do agendamento

---
+++ app/src/main/java/com/example/clinicproject/CoverPage.java
@@ -0,0 +1,32 @@
+package com.example.clinicproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CoverPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cover_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void iniciar(View view){
+        Intent tela_inicial = new Intent(this,MainActivity.class);
+        startActivity(tela_inicial);
+
+    }
+}
