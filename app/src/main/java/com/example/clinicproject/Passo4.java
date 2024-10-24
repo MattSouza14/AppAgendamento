5c3446659de720e9cd4c990969486fe3bdda7760
MattSouza14 <mateussouzabr1@gmail.com>
2024-10-24 00:46:37 -0300

    Aletações de layouts

--- app/src/main/java/com/example/clinicproject/Passo4.java
+++ app/src/main/java/com/example/clinicproject/Passo4.java
@@ -3,7 +3,8 @@ package com.example.clinicproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -22,7 +23,34 @@ public class Passo4 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Spinner spinnerGenero = findViewById(R.id.spinnerGenero);
+        String[] generos = {
+                "Escolha uma genero",
+                "Masculino",
+                "Feminino"
+        };
+        ArrayAdapter<String> genero = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, generos);
+        genero.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGenero.setAdapter(genero);
+
+
+
+        Spinner spinnerEstados = findViewById(R.id.spinnerEstados);
+        String[] estados = {
+                "Escolha uma estado!",
+                "São Paulo",
+                "Rio de Janeiro",
+                "Minas Gerais",
+                "Bahia",
+                "Paraná",
+                "Rio Grande do Sul",
+                "Ceará"
+        };
+        ArrayAdapter<String> estado = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, estados);
+        estado.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEstados.setAdapter(estado);
     }
+
 
     public void telaAnterior4(View view){
         Intent primeira_tela = new Intent(this,Passo3.class);
