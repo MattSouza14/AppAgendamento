ada1838d433874cd95543db6b339c37151716851
MattSouza14 <mateussouzabr1@gmail.com>
2024-11-01 22:33:31 -0300

    Finalização de layout

--- app/src/main/java/com/example/clinicproject/Passo5.java
+++ app/src/main/java/com/example/clinicproject/Passo5.java
@@ -1,9 +1,9 @@
 package com.example.clinicproject;
 
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,42 +16,50 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Passo5 extends AppCompatActivity {
     PatientInfo paciente;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_passo5);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
+
+
         Intent intent = getIntent();
         paciente = (PatientInfo) intent.getSerializableExtra("paciente");
 
-           TextView nomeCompletoText = findViewById(R.id.nomeUser);
-           nomeCompletoText.setText(paciente.getCompletName());
-           TextView cpfText = findViewById(R.id.cpfUser);
-           cpfText.setText(paciente.getCpf());
-           TextView celularText = findViewById(R.id.celularUser);
-           celularText.setText(paciente.getCellPhoneNumber());
+        TextView nomeCompletoText = findViewById(R.id.nomeUser);
+        nomeCompletoText.setText(paciente.getCompletName());
 
+        TextView cpfText = findViewById(R.id.cpfUser);
+        cpfText.setText(paciente.getCpf());
 
-
-
+        TextView celularText = findViewById(R.id.celularUser);
+        celularText.setText(paciente.getCellPhoneNumber());
     }
 
-
-    public void telaAnterior5(View view){
-        Intent tela4 = new Intent(this,Passo4.class);
+    public void telaAnterior5(View view) {
+        Intent tela4 = new Intent(this, Passo4.class);
         startActivity(tela4);
-
     }
+
     public void confirmarConsulta(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_dialog, null);
+        TextView title = dialogView.findViewById(R.id.dialog_title);
+        title.setText("Consulta Agendada");
+        TextView message = dialogView.findViewById(R.id.dialog_message);
+        message.setText("Sua consulta foi agendada com sucesso!");
+
+
         new AlertDialog.Builder(this)
-                .setTitle("Consulta Agendada")
-                .setMessage("Sua consulta foi agendada com sucesso!")
+                .setView(dialogView)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
